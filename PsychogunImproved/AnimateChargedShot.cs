19419f058fb934382a36d95f0ca4b31230670417
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-06-20 00:47:02 -0400

    Major improvements to charged psychogun shots

---
+++ PsychogunImproved/AnimateChargedShot.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace PsychogunImproved;
+
+public class AnimateChargedShot : MonoBehaviour
+{
+    private static readonly Color Blue = new(0f, 0.2f, 5.4f);
+    private static readonly Color Yellow = new(1.274f, 1.5f, 0.445f);
+    private static readonly Color Red = new(3.674f, 0.1f, 0.145f);
+
+    public Renderer renderer;
+    
+    private float _startTime;
+    private Color _defaultColor;
+    private Material _material;
+    
+    private static readonly Frame[] Animation =
+    [
+        new(0f, Yellow),
+        new(0.05f, Blue),
+        new(0.11f, Red),
+        Frame.GetDefault(0.17f)
+    ];
+
+    private static readonly int ColorProperty = Shader.PropertyToID("Color_D9B06DDF");
+
+    private void Start()
+    {
+        _startTime = Time.time;
+        _material = renderer.material;
+        _defaultColor = _material.GetColor(ColorProperty);
+    }
+
+    private void Update()
+    {
+        _material.SetColor(ColorProperty, GetColorForFrame(GetCurrentFrame()));
+    }
+
+    private Color GetColorForFrame(Frame frame)
+    {
+        if (frame.UseDefault) return _defaultColor;
+        return frame.Color;
+    }
+
+    private Frame GetCurrentFrame()
+    {
+        var time = Time.time - _startTime;
+        for (int i = Animation.Length - 1; i >= 0; i--)
+        {
+            if (time >= Animation[i].Time)
+            {
+                return Animation[i];
+            }
+        }
+
+        return default;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(_material);
+    }
+
+    private struct Frame
+    {
+        public float Time { get; }
+        public Color Color { get; }
+        public bool UseDefault { get; }
+
+        public Frame(float time, Color color) : this(time, color, false)
+        {
+        }
+
+        private Frame(float time, Color color, bool useDefault)
+        {
+            Time = time;
+            Color = color;
+            UseDefault = useDefault;
+        }
+        
+        public static Frame GetDefault(float time) => new(time, Color.white, true);
+    }
+}
--- PsychogunImproved/Patches.cs
+++ PsychogunImproved/Patches.cs
@@ -7,6 +7,10 @@ namespace PsychogunImproved;
 [HarmonyPatch]
 public static class Patches
 {
+    private static bool _upgradedPsychogunShotDirty = false;
+
+    private static string _chargedPsychogunPrefabName;
+    
     [HarmonyPostfix]
     [HarmonyPatch(typeof(CobraCharacter), nameof(CobraCharacter.Start))]
     public static void StartPostfix(CobraCharacter __instance)
@@ -21,11 +25,13 @@ public static class Patches
         armAnimation.transitionDuration = 0.1f;
         
         behaviour.animation = armAnimation;
+
+        _chargedPsychogunPrefabName = __instance.dependencies.chargedShot.name;
     }
 
     [HarmonyPrefix]
     [HarmonyPatch(typeof(CobraCharacter), nameof(CobraCharacter.ShootPsychogun))]
-    public static bool TestPatch(CobraCharacter __instance, Vector2 dir)
+    public static bool AlwaysShootChargedShotPatch(CobraCharacter __instance, Vector2 dir)
     {
         var behaviour = __instance.gameObject.GetComponent<ImprovedPsychogunBehaviour>();
         if (behaviour == null)
@@ -38,6 +44,7 @@ public static class Patches
         {
             __instance.ShootChargedShot(dir);
             behaviour.StartShotCooldown();
+            _upgradedPsychogunShotDirty = true;
         }
 
         return false;
@@ -45,7 +52,7 @@ public static class Patches
     
     [HarmonyPostfix]
     [HarmonyPatch(typeof(CobraCharacter), nameof(CobraCharacter.ShootChargedShot))]
-    public static void ShootChargedShotPatch(CobraCharacter __instance)
+    public static void PlayShootRecoilAnimation(CobraCharacter __instance)
     {
         var behaviour = __instance.gameObject.GetComponent<ImprovedPsychogunBehaviour>();
         if (behaviour == null)
@@ -55,5 +62,84 @@ public static class Patches
         }
 
         behaviour.PlayShootAnimation();
+        _upgradedPsychogunShotDirty = false;
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ParticleController), nameof(ParticleController.Generate), typeof(GameObject),
+        typeof(GameObject), typeof(bool), typeof(bool), typeof(GameObject), typeof(string))]
+    public static void FullyChargedShotParticlePatch(GameObject __result, GameObject particle)
+    {
+        if (_upgradedPsychogunShotDirty || !particle.name.Equals(_chargedPsychogunPrefabName))
+        {
+            return;
+        }
+
+        Plugin.Logger.LogInfo("Patching charged shot particle for fully charged shot");
+
+        try
+        {
+            __result.GetComponent<MeshRenderer>().enabled = false;
+            
+            var projectile = __result.GetComponent<Projectile>();
+            projectile.defaultReaction = Damage.Reaction.PassThrough;
+
+            const float radiusMultiplier = 6f;
+            
+            projectile.speed = 60;
+            projectile.overlapSphereRadiusAtInit *= radiusMultiplier;
+            projectile.capsuleCastRadiusAtInitAndUpdate *= radiusMultiplier;
+            
+            
+            var collider = __result.GetComponent<SphereCollider>();
+            collider.radius *= radiusMultiplier;
+            
+            __result.transform.Find("trail").GetComponent<TrailRenderer>().enabled = false;
+            __result.transform.Find("Point Light").GetComponent<Light>().enabled = false;
+            
+            var flipbook = __result.transform.Find("vfx_charge_trail_00/vfx_ps_flipbook_00");
+            var particleSystem = flipbook.GetComponent<ParticleSystem>();
+            var shape = particleSystem.shape;
+            shape.position = new Vector3(0f, 0f, 10.5f);
+            var main = particleSystem.main;
+            main.startSizeXMultiplier = 22; // original value is 6
+            main.startSizeYMultiplier = 4;
+            // this animation will apply to all
+            flipbook.gameObject.AddComponent<AnimateChargedShot>().renderer = particleSystem.GetComponent<Renderer>();
+            
+            var longFlipbook = Object.Instantiate(flipbook, flipbook.transform.parent, true);
+            var longParticleSystem = longFlipbook.GetComponent<ParticleSystem>();
+            var longShape = longParticleSystem.shape;
+            longShape.position = new Vector3(0f, 0f, 21f);
+            var longMain = longParticleSystem.main;
+            longMain.startSizeXMultiplier = 40;
+            longMain.startSizeYMultiplier = 4;
+            var longLight = longFlipbook.GetComponentInChildren<Light>();
+            if (longLight) longLight.enabled = false;
+            
+            var wideFlipbook = Object.Instantiate(flipbook, flipbook.transform.parent, true);
+            var wideParticleSystem = wideFlipbook.GetComponent<ParticleSystem>();
+            var wideShape = wideParticleSystem.shape;
+            wideShape.position = new Vector3(0f, 0f, 2f);
+            var wideMain = wideParticleSystem.main;
+            wideMain.startSizeXMultiplier = 5; // original value is 6
+            wideMain.startSizeYMultiplier = 6.3f; // original value is 4
+            var wideLight = wideFlipbook.GetComponentInChildren<Light>();
+            if (wideLight) wideLight.enabled = false;
+
+            flipbook.GetComponent<ParticleSystemRenderer>().maxParticleSize = 4;
+            longFlipbook.GetComponent<ParticleSystemRenderer>().maxParticleSize = 5;
+        }
+        catch (System.Exception e)
+        {
+            Plugin.Logger.LogError("Exception thrown while patching fully charged shot particle: " + e);
+        }
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CobraCharacter), nameof(CobraCharacter.ShootSub))]
+    public static void CleanUpDirtyCheck(CobraCharacter __instance)
+    {
+        _upgradedPsychogunShotDirty = false;
     }
 }
