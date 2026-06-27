5c2b94bc20efabe5a964650c64dbf36b2d903114
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-06-27 03:01:11 -0400

    Add music restart fix mod

---
+++ MusicRestartFix/Patches.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+
+namespace MusicRestartFix;
+
+[HarmonyPatch]
+public static class Patches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NUIMainMenu), nameof(NUIMainMenu.Start))]
+    public static void MainMenuStartPostfix()
+    {
+        KillStaleAudio();
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(NUILevelSelectionPanel), nameof(NUILevelSelectionPanel.Start))]
+    public static void LevelSelectionPanelStartPostfix()
+    {
+        KillStaleAudio();
+    }
+
+    private static void KillStaleAudio()
+    {
+        var sources = AudioController.Audio.audioSrc;
+        foreach (var source in sources)
+        {
+            if (source.volTarget == 0)
+            {
+                Plugin.Logger.LogDebug("Stopping unused song");
+                source.asrc.Stop();
+            }
+        }
+    }
+}
