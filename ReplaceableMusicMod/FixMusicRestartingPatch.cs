8ff217aace51f1452ab4b11bb6cfbc7e15f8b06d
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-07-23 22:37:10 -0400

    Remove broken music restart patch

--- ReplaceableMusicMod/FixMusicRestartingPatch.cs
+++ ReplaceableMusicMod/FixMusicRestartingPatch.cs
@@ -1,4 +1,6 @@
+using System;
 using HarmonyLib;
+using UnityEngine;
 
 namespace MusicReplacer;
 
@@ -11,4 +13,31 @@ public static class FixMusicRestartingPatch
     {
         _forcerestartmusic = false;
     }
+
+    /*
+    private static float _timeCleanDeadMusicAgain;
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(AudioController), nameof(AudioController.Update))]
+    public static void UpdatePostfix(AudioController __instance)
+    {
+        if (Time.time < _timeCleanDeadMusicAgain)
+            return;
+        _timeCleanDeadMusicAgain = Time.time + 1;
+
+        var sources = AudioController.Audio.audioSrc;
+        foreach (var source in sources)
+        {
+            if (source.type != CAudio.eVolumeType.music)
+                continue;
+            if (source.asrc == null)
+                continue;
+            if (source.asrc.clip == null ||
+                source.asrc.clip.name.Equals("mus_loop_main_theme", StringComparison.OrdinalIgnoreCase))
+                return;
+            if (source.asrc.volume == 0)
+                source.asrc.Stop();
+        }
+    }
+    */
 }
