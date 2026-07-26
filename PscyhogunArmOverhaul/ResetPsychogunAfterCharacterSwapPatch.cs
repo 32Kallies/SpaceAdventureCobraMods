c5bf0d48768fb091be2fdcfdefa0f83f42abec09
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-07-26 03:07:32 -0400

    Fix issue after swapping skins

---
+++ PscyhogunArmOverhaul/ResetPsychogunAfterCharacterSwapPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace PscyhogunArmOverhaul;
+
+[HarmonyPatch]
+public static class ResetPsychogunAfterCharacterSwapPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(NUICharacterPanel), nameof(NUICharacterPanel.OnDestroy))]
+    private static void OnBeforeCloseSkinSelectionMenu(NUICharacterPanel __instance)
+    {
+        // if the skin was changed, based on logic from original function
+        if (LoadSaveController.Instance.PreferencesData.currentCobraSkin != __instance.m_AvailableSkins[__instance.m_CurrentSkinIndex])
+        {
+            // reset psychogun state for new skin
+            var state = PsychogunStateRememberer.GetInstance(true);
+            if (state != null)
+            {
+                state.SetToken(Token.HardCodedTokens.ForcePsychogunOff, true);
+                state.SetToken(Token.HardCodedTokens.ForcePsychogunOn, false);
+            }
+        }
+    }
+}
