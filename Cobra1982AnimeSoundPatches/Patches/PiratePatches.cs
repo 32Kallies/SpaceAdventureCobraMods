0403712133bc428e383223da3f4191a17395c34a
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-08-05 21:59:21 -0400

    Fix large vehicle enemy at end of 5-2

--- Cobra1982AnimeSoundPatches/Patches/PiratePatches.cs
+++ Cobra1982AnimeSoundPatches/Patches/PiratePatches.cs
@@ -5,11 +5,9 @@ using JetBrains.Annotations;
 
 namespace Cobra1982AnimeSoundPatches.Patches;
 
-[HarmonyPatch(typeof(NmiPirate))]
-[HarmonyPatch(nameof(NmiPirate.LateUpdate))]
-public static class PiratePatches
+public static class PiratePatchUtils
 {
-    private static int GetPirateShootSoundClip()
+    internal static int GetPirateShootSoundClip()
     {
         if (CobraSoundReplacer.API.CustomSoundUtils.TryGetEClip("square_vehicle_shot", out var clip))
         {
@@ -20,7 +18,11 @@ public static class PiratePatches
         Plugin.Logger.LogError("Failed to find 'square_vehicle_shot' eClip for replacing pirate vehicle sound");
         return 188; // use the old default sound
     }
-    
+}
+[HarmonyPatch(typeof(NmiPirate))]
+[HarmonyPatch(nameof(NmiPirate.LateUpdate))]
+public static class PirateLateUpdatePatches
+{
     // Silences the original shoot volume
     [UsedImplicitly]
     private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
@@ -36,7 +38,7 @@ public static class PiratePatches
 
             if (instruction.opcode == OpCodes.Ldc_I4 && (int)instruction.operand == 188)
             {
-                yield return CodeInstruction.Call(typeof(PiratePatches), nameof(GetPirateShootSoundClip));
+                yield return CodeInstruction.Call(typeof(PiratePatchUtils), nameof(PiratePatchUtils.GetPirateShootSoundClip));
                 done = true;
                 continue;
             }
@@ -46,11 +48,48 @@ public static class PiratePatches
 
         if (!done)
         {
-            Plugin.Logger.LogError("Failed to patch pirate shoot sound");
+            Plugin.Logger.LogError("Failed to patch pirate shoot sound in LateUpdate");
         }
         else
         {
-            Plugin.Logger.LogInfo("Patched pirate shoot sound");
+            Plugin.Logger.LogInfo("Patched pirate shoot sound in LateUpdate");
+        }
+    }
+}
+[HarmonyPatch(typeof(NmiPirate))]
+[HarmonyPatch(nameof(NmiPirate.Update))]
+public static class PirateUpdatePatches
+{
+    // Silences the original shoot volume
+    [UsedImplicitly]
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        bool done = false;
+        foreach (var instruction in instructions)
+        {
+            if (done)
+            {
+                yield return instruction;
+                continue;
+            }
+
+            if (instruction.opcode == OpCodes.Ldc_I4 && (int)instruction.operand == 188)
+            {
+                yield return CodeInstruction.Call(typeof(PiratePatchUtils), nameof(PiratePatchUtils.GetPirateShootSoundClip));
+                done = true;
+                continue;
+            }
+            
+            yield return instruction;
+        }
+
+        if (!done)
+        {
+            Plugin.Logger.LogError("Failed to patch pirate shoot sound in update");
+        }
+        else
+        {
+            Plugin.Logger.LogInfo("Patched pirate shoot sound in update");
         }
     }
 }
