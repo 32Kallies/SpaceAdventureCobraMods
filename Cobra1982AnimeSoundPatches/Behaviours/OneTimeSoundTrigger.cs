738dafed700b67bd2aebaca0aad296e016a0930d
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-08-13 02:25:49 -0400

    Create OneTimeSoundTrigger

---
+++ Cobra1982AnimeSoundPatches/Behaviours/OneTimeSoundTrigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Cobra1982AnimeSoundPatches.Behaviours;
+
+public class OneTimeSoundTrigger : MonoBehaviour
+{
+    public audioSelectionData.eCLIP clip;
+
+    private Collider _collider;
+    private bool _played;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    private void OnDestroy()
+    {
+        _collider = null;
+    }
+
+    private void Update()
+    {
+        if (_played) return;
+        
+        if (LevelController.Instance != null && CobraCharacter.Instance != null && audioReverbTrigger.IsPointInCollider(_collider, CobraCharacter.Instance.transform.position))
+        {
+            AudioController.Instance.PlaySound(clip);
+            _played = true;
+        }
+    }
+}
---
+++ Cobra1982AnimeSoundPatches/Utility/OneTimeSoundTriggerCreator.cs
@@ -0,0 +1,30 @@
+﻿using Cobra1982AnimeSoundPatches.Behaviours;
+using CobraSoundReplacer.API;
+using UnityEngine;
+
+namespace Cobra1982AnimeSoundPatches.Utility;
+
+public static class OneTimeSoundTriggerCreator
+{
+    public static void CreateCustomTrigger(string name, Vector3 center, Vector3 size, string clipName)
+    {
+        var gameObject = new GameObject(name);
+        gameObject.SetActive(false);
+        
+        gameObject.layer = 12;
+        var collider = gameObject.AddComponent<BoxCollider>();
+        collider.isTrigger = true;
+        var behavior = gameObject.AddComponent<OneTimeSoundTrigger>();
+        if (!CustomSoundUtils.TryGetEClip(clipName, out var clip))
+        {
+            Plugin.Logger.LogWarning("Failed to find clip by name " + clipName);
+            return;
+        }
+        behavior.clip = clip;
+        
+        gameObject.transform.position = center;
+        collider.size = size;
+        
+        gameObject.SetActive(true);
+    }
+}
