acb7fb0315b3ff98366a684e605dcf4577490fe1
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-08-13 02:26:00 -0400

    Add turtle elevator and reveal sfx to 3-1

---
+++ Cobra1982AnimeSoundPatches/Patches/LevelSpecificPatches.cs
@@ -0,0 +1,23 @@
+﻿using Cobra1982AnimeSoundPatches.Utility;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Cobra1982AnimeSoundPatches.Patches;
+
+[HarmonyPatch]
+public static class LevelSpecificPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(LevelController), nameof(LevelController.Start))]
+    private static void StartPostfix(LevelController __instance)
+    {
+        LevelController.Level level = __instance.level;
+        
+        if (level == LevelController.Level.EP03_LVL01_Graveyard)
+        {
+            OneTimeSoundTriggerCreator.CreateCustomTrigger(
+                "TurtleDiscoveryElevatorTrigger", new Vector3(1149, -115, 0),
+                new Vector3(3, 2.5f, 3), "turtle_elevator_and_reveal");
+        }
+    }
+}
