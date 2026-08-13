2fc602fb062c91a754f6165ae4d5a4be0cd60257
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-08-13 00:58:29 -0400

    Disable music playing after Zigova boss fight

---
+++ Cobra1982AnimeSoundPatches/Patches/Bosses/ZigovaDisableMusicPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Cobra1982AnimeSoundPatches.Utility;
+using HarmonyLib;
+
+namespace Cobra1982AnimeSoundPatches.Patches.Bosses;
+
+[HarmonyPatch]
+public static class ZigovaDisableMusicPatch
+{
+    private const string VideoNameToDisableMusic = "CS2D_V_2_3_2";
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CobraVideoPlayer), nameof(CobraVideoPlayer.Stop))]
+    private static void OnVideoEnd(CobraVideoPlayer __instance)
+    {
+        if (!__instance.videoName.Equals(VideoNameToDisableMusic, StringComparison.OrdinalIgnoreCase))
+            return;
+
+        DisableMusicTriggerUtils.DisableMusicTriggersAroundCobra();
+    }
+}
---
+++ Cobra1982AnimeSoundPatches/Utility/DisableMusicTriggerUtils.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Cobra1982AnimeSoundPatches.Utility;
+
+public static class DisableMusicTriggerUtils
+{
+    public static void DisableMusicTriggersAroundCobra()
+    {
+        var cobra = CobraCharacter.Instance;
+        if (cobra == null)
+        {
+            Plugin.Logger.LogWarning("Cobra does not exist; can't disable music trigger");
+            return;
+        }
+
+        int disabled = 0;
+        var colliders = Physics.OverlapSphere(cobra.transform.position, 2);
+        foreach (var collider in colliders)
+        {
+            if (collider.GetComponent<audioForceMusicTrigger>() != null)
+            {
+                collider.gameObject.SetActive(false);
+                disabled++;
+            }
+        }
+        Plugin.Logger.LogInfo($"Disabled {disabled} music triggers around Cobra");
+    }
+}
